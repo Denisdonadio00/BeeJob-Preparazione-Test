030b14d738554b76dea7cf6a413b76ebffa8df99
Denisdonadio00 <144681826+Denisdonadio00@users.noreply.github.com>
2026-05-07 12:18:43 +0200

    Primo Commit
    Test sperimentale

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'beejob-v1';
+const assets = [
+    'beestudy.html',
+    'manifest.json'
+];
+
+// Installazione: salva i file nella cache
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(assets);
+        })
+    );
+});
+
+// Fetch: serve i file dalla cache se offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
